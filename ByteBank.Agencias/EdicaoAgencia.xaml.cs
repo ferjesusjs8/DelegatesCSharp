84d305d652f2e0191f4e5f7c0fc3d2e8155f23e2
fernando.js8 <fernando.js8@outlook.com>
2019-08-04 10:37:50 -0300

    initial commit

---
+++ ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Windows;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for EdicaoAgencia.xaml
+    /// </summary>
+    public partial class EdicaoAgencia : Window
+    {
+
+        private readonly Agencia _agencia;
+        public EdicaoAgencia(Agencia agencia)
+        {
+            InitializeComponent();
+
+            _agencia = agencia ?? throw new ArgumentNullException(nameof(agencia));
+            AtualizarCamposDeTexto();
+            AtualizarControles();
+        }
+
+        private void AtualizarCamposDeTexto()
+        {
+            txtNumero.Text = _agencia.Numero;
+            txtNome.Text = _agencia.Nome;
+            txtDescricao.Text = _agencia.Descricao;
+            txtEndereco.Text = _agencia.Endereco;
+            txtTelefone.Text = _agencia.Telefone;
+        }
+
+        private void AtualizarControles()
+        {
+            var okEventHandler = (RoutedEventHandler)btnOk_Click + Fechar;
+            var cancelarEventHandler =
+                (RoutedEventHandler)Delegate.Combine(
+                    (RoutedEventHandler)btnCancelar_Click,
+                    (RoutedEventHandler)Fechar);
+
+            btnOk.Click += new RoutedEventHandler(okEventHandler);
+            btnCancelar.Click += new RoutedEventHandler(cancelarEventHandler);
+
+            btnOk.Click += new RoutedEventHandler(Fechar);
+            btnCancelar.Click += new RoutedEventHandler(Fechar);
+        }
+
+        private void btnOk_Click(object sender, EventArgs e) => DialogResult = true;
+
+        private void btnCancelar_Click(object sender, EventArgs e) => DialogResult = false;
+
+        private void Fechar(object sender, EventArgs e) => Close();
+    }
+}
---
+++ ByteBank.Agencias/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using ByteBank.Agencias.DAL;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ByteBankEntities _contextoBancoDeDados = new ByteBankEntities();
+        private readonly ListBox lstAgencias;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lstAgencias = new ListBox();
+            AtualizarControles();
+            AtualizarListaAgencias();
+        }
+
+        private void AtualizarControles()
+        {
+            lstAgencias.Width = 270;
+            lstAgencias.Height = 290;
+
+            Canvas.SetTop(lstAgencias, 15);
+            Canvas.SetLeft(lstAgencias, 15);
+
+            lstAgencias.SelectionChanged += new SelectionChangedEventHandler(lstAgencias_SelectionChanged);
+
+            container.Children.Add(lstAgencias);
+
+            btnEditar.Click += new RoutedEventHandler(btnEditar_Click);
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            var agenciaAtual = (Agencia)lstAgencias.SelectedItem;
+
+            var janelaEdicao = new EdicaoAgencia(agenciaAtual);
+            var resultado = janelaEdicao.ShowDialog().Value;
+
+            if (resultado)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void AtualizarListaAgencias()
+        {
+            lstAgencias.Items.Clear();
+            var agencias = _contextoBancoDeDados.Agencias.ToList();
+            foreach (var agencia in agencias)
+                lstAgencias.Items.Add(agencia);
+        }
+
+        private void lstAgencias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var agenciaSelecionada = (Agencia)lstAgencias.SelectedItem;
+
+            txtNumero.Text = agenciaSelecionada.Numero;
+            txtNome.Text = agenciaSelecionada.Nome;
+            txtTelefone.Text = agenciaSelecionada.Telefone;
+            txtEndereco.Text = agenciaSelecionada.Endereco;
+            txtDescricao.Text = agenciaSelecionada.Descricao;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var confirmacao = 
+                MessageBox.Show(
+                    "Você deseja realmente excluir este item?",
+                    "Confirmação",
+                    MessageBoxButton.YesNo);
+            if (confirmacao == MessageBoxResult.Yes)
+            {
+                //Excluir
+            }
+            else
+            {
+                //Não faz nada
+            }
+        }
+    }
+}
